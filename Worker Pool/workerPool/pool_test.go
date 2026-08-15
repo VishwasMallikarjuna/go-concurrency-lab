5dd0289b8da5f7278750ab2bfc576556c03e4857
VISHWAS DIBBUR MALLIKARJUNA <73868815+VishwasMallikarjuna@users.noreply.github.com>
2026-08-15 20:40:52 +0530

    Update pool_test.go

--- Worker Pool/workerPool/pool_test.go
+++ Worker Pool/workerPool/pool_test.go
@@ -29,3 +29,35 @@ func TestPool_ExecutesJobs(t *testing.T) {
 		t.Fatalf("executed = %d, want 10", got)
 	}
 }
+
+func TestPool_ExecutesConcurrently(t *testing.T) {
+	pool := New(context.Background(), 3, 10)
+	defer pool.Shutdown()
+
+	started := make(chan struct{}, 3)
+	release := make(chan struct{})
+
+	for i := 0; i < 3; i++ {
+		err := pool.Submit(func(ctx context.Context) error {
+			started <- struct{}{}
+
+			<-release
+
+			return nil
+		})
+
+		if err != nil {
+			t.Fatalf("Submit() error = %v", err)
+		}
+	}
+
+	for i := 0; i < 3; i++ {
+		select {
+		case <-started:
+		case <-time.After(time.Second):
+			t.Fatal("workers did not execute concurrently")
+		}
+	}
+
+	close(release)
+}
